5a72c583c81520d2826093cb9fd72cb9c8ae7abc
canbe <canbe@CANBERK>
2024-08-10 16:53:44 +0300

    Add project files.

---
+++ OOPPracticeLibrary/Library.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticeLibrary
+{
+    public class Library
+    {
+        public string _bookName;
+        public string _authorName;
+        public string _authorSurname;
+        public int _pageNum;
+        public string _publisher;
+
+        public DateTime _logDate;
+
+
+        public string Bookname
+        {
+            get
+            {
+                return _bookName;
+            }
+
+
+            set
+            {
+                _bookName = value;
+            }
+
+        }
+
+        public string AuthorName 
+        
+        {
+            get
+
+            {
+                return _authorName;
+            }
+
+            set
+            {
+                _authorName = value;
+            }
+
+
+        
+        }
+
+
+        public string AuthorSurname
+        {
+            get 
+            
+            {
+                return _authorSurname;
+            
+            }
+
+            set
+            {
+                _authorSurname = value;
+            }
+        }
+        public int PageNum
+
+        {
+            get
+            {
+                return _pageNum;
+            }
+
+
+
+            set
+            {
+                _pageNum = value;
+            }
+        }
+
+            public string Publisher
+        {
+
+            get
+            {
+                return _publisher;
+            }
+
+
+            set
+            {
+                _publisher = value;
+            }
+
+
+        }
+
+            public Library()
+        {
+            Date();
+        }
+
+
+        public Library(string bookName, string authorName, string authorSurname,int pageNum, string publisher)
+        {
+            _bookName=bookName;
+            _authorName=authorName; 
+            _authorSurname=authorSurname;
+            _pageNum = pageNum;
+            _publisher=publisher;
+
+        }
+
+        public void Date()
+        {
+            _logDate=DateTime.Now;
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"Book Name: {_bookName}\nAuthor Name: {_authorName}\nAuthorSurname: {_authorSurname}\nPage Number: {_pageNum}\nPublisher: {_publisher}\n" );
+        }
+
+
+
+    }
+
+    }
+
---
+++ OOPPracticeLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿using OOPPracticeLibrary;
+
+Library book1 = new Library();
+book1 = new Library("Adı Aylin", "Ayşe", "Kulin",398 ,"Remzi Kitabevi" );
+
+Library book2 = new Library();
+
+book2.Bookname = "Modern Ortadoğu Tarihi";
+book2._authorName = "William Lennon";
+book2._authorSurname = "Cleveland";
+book2.PageNum = 634;
+book2.Publisher = "Agora Kitaplığı";
+
+
+book1.DisplayProduct();
+book2.DisplayProduct();
+
+
+
+/*
+1. Sınıflar (Classes)
+OOP'de sınıf, bir nesnenin şablonudur. İçinde özellikler ve davranışlar tanımlanır. Nesneler, bu sınıflardan oluşturulur.
+
+2. Özellikler (Properties)
+Özellikler, bir nesnenin durumunu temsil eden verilere kontrollü erişim sağlar. get ve set erişimcileri ile veriler okunabilir veya yazılabilir.
+
+3. new Anahtar Kelimesi
+new anahtar kelimesi, bir sınıftan yeni bir nesne oluşturur ve bellekte bu nesne için yer ayırır.
+
+4. Yapıcılar (Constructors)
+Yapıcılar, bir nesne ilk oluşturulduğunda çalışan özel metotlardır. Nesnenin başlangıç durumunu ayarlamak için kullanılırlar.
+*/
+
